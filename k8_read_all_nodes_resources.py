d7e8af59113033c4654d74f080c5afd19a11f92a
prasadjjoshi <prasad.joshi@pivotus.com>
2019-09-18 16:51:17 -0700

    refactored

--- k8_read_all_nodes_resources.py
+++ k8_read_all_nodes_resources.py
@@ -2,7 +2,6 @@
 
 import os
 import json
-import fileinput
 
 DESCRIBE_COMMAND_OUTPUT_FILE = 'nodes.log'
 node_properties = {}
@@ -17,49 +16,68 @@ lines = open(describe_command_output_file, "r")
 node_name_dict = {}
 node_name = ""
 
+
+def get_name():
+    global line, node_name, node_name_dict
+    line = line.split(":")
+    node_name = line[1].strip()
+    if not node_properties.__contains__(node_name):
+        node_properties[node_name] = None
+    node_name_dict = {line[0].strip(): line[1].strip()}
+
+
+def get_cpu():
+    global line, cpu_res
+    line = line.split()
+    cpu = line[0]
+    cpu_res = {
+        cpu:
+            {requests_key: line[1] + " " + line[2], limits_key: line[3] + " " + line[4]}
+    }
+
+
+def get_memory():
+    global line, mem_res
+    line = line.split()
+    mem = line[0]
+    mem_res = {
+        mem:
+            {requests_key: line[1] + " " + line[2], limits_key: line[3] + " " + line[4]}
+    }
+
+
+def get_allocated_resources():
+    global line, cpu_res, mem_res, requests_key, limits_key
+    line = lines.__next__()
+    resources = ""
+    cpu_res = {}
+    mem_res = {}
+    requests_key = ""
+    limits_key = ""
+    while not line.startswith("Events"):
+
+        if line.strip().startswith("Resource"):
+            line = line.split()
+            resource_key = line[0]
+            requests_key = line[1]
+            limits_key = line[2]
+
+        elif line.strip().startswith('cpu'):
+            get_cpu()
+
+        elif line.strip().startswith('memory'):
+            get_memory()
+
+        line = lines.__next__()  # iterating till events to get all resources
+
+
 if lines.__sizeof__() > 0:
     for line in lines:
         if line.startswith("Name"):
-            line = line.split(":")
-            node_name = line[1].strip()
-            if not node_properties.__contains__(node_name):
-                node_properties[node_name] = None
-
-            node_name_dict = {line[0].strip(): line[1].strip()}
+            get_name()
 
         elif line.startswith(ALLOCATED_RESOURCES):
-            line = lines.__next__()
-            resources = ""
-            cpu_res = {}
-            mem_res = {}
-            requests_key = ""
-            limits_key = ""
-
-            while not line.startswith("Events"):
-
-                if line.strip().startswith("Resource"):
-                    line = line.split()
-                    resource_key = line[0]
-                    requests_key = line[1]
-                    limits_key = line[2]
-
-                elif line.strip().startswith('cpu'):
-                    line = line.split()
-                    cpu = line[0]
-                    cpu_res = {
-                                cpu:
-                                    {requests_key: line[1]+" "+line[2], limits_key: line[3]+" "+line[4]}
-                            }
-
-                elif line.strip().startswith('memory'):
-                    line = line.split()
-                    mem = line[0]
-                    mem_res = {
-                        mem:
-                            {requests_key: line[1] + " " + line[2], limits_key: line[3] + " " + line[4]}
-                    }
-
-                line = lines.__next__() # iterating till events to get all resources
+            get_allocated_resources()
 
             all_props_of_node = [node_name_dict, cpu_res, mem_res]
             node_properties[node_name] = all_props_of_node
